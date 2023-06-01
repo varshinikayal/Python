a483bcea024d5d1f8674b720769969ec696841b3
varshinikayal <135025163+varshinikayal@users.noreply.github.com>
2023-06-01 14:56:15 +0530

    dividetwointegers.py

---
+++ dividetwointegers.py
@@ -0,0 +1,19 @@
+#function to divide a by b a and return floor value
+def divide(dividend,divisor):
+  #calculate the sign of divisor
+  #sign will be negative only if either one of them is negative
+  # otherwise it will be positive
+  sign = -1 if ((divident<0) ^ (divisor < 0)) else 1
+  #update both the divisor and dividend positive
+  dividend = abs (dividend)
+  divisor = abs (divisor)
+  #Initialize the quotient
+  quotient = 0
+ while (dividend >= divisor):
+    dividend -= divisor
+    quotient += 1
+    #If the sign value is -1 then
+    #negate the value of quotient
+ if sign == -1:
+    quotient = - quotient
+ return quotient
