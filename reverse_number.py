734f8d25f62a0e4890e9a7dcc74ff81056527739
varshinikayal <135025163+varshinikayal@users.noreply.github.com>
2023-06-01 15:46:11 +0530

    reverse_number.py
    Reverse Integer
     Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.

---
+++ reverse_number.py
@@ -0,0 +1,10 @@
+#the numbers to be reversed
+number = 963
+#convert the number to string
+num_str = str(number)
+#store the size of the number
+size=len(num_str)
+#to reverse the number use slicing
+reversed_num = num_string[size::-1]
+#print the output
+print("The Reversed number is : " + reversed_num)
